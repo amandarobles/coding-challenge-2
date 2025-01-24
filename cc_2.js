71da30e3102c4764cfc52e8bd584cc686d7f34af
Amanda Robles <amandarobles@usf.edu>
2025-01-23 20:40:27 -0500

    Task 2 - Student Scores

--- cc_2.js
+++ cc_2.js
@@ -3,3 +3,11 @@ let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
 products.push("Tablet");
 products.pop();
 console.log("Updated Products List:", products);
+
+//task 2 - Accessing and Modifying Arrays
+let scores = [85, 90, 78, 92, 88];
+scores[1] = 95;
+let total = scores.reduce((sum, score) => sum + score, 0);
+let average = total / scores.length;
+console.log("Updated Scores:", scores);
+console.log("Average Score:", average);
