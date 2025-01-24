f80e86db120c24d74ad4e3df97f429627656899a
Amanda Robles <amandarobles@usf.edu>
2025-01-23 20:35:23 -0500

    Task 1- Store Inventory

---
+++ cc_2.js
@@ -0,0 +1,5 @@
+//task 1 - Working with Arrays
+let products = ["Laptop", "Mouse", "Keyboard", "Monitor", "Printer"];
+products.push("Tablet");
+products.pop();
+console.log("Updated Products List:", products);
